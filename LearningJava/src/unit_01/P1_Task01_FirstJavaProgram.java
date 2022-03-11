b0c925f15a684eb2cf55b62a1d01802c058ed14f
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-03-11 14:25:31 +0530

    BasicPrograms

--- LearningJava/src/unit_01/P1_Task01_FirstJavaProgram.java
+++ LearningJava/src/unit_01/P1_Task01_FirstJavaProgram.java
@@ -4,6 +4,8 @@ public class P1_Task01_FirstJavaProgram {
 
 	public static void main(String[] args) {
 		   System.out.println("Hello World!!!");
+		   System.out.println("Hello World!!!");
+		   System.out.println("Hello World!!!");
 
 	}
 
