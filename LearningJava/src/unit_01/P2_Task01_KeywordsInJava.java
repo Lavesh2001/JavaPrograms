dab3bd7d2b9c2fe1a769985af9b3bc1143c8a87c
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-03-25 15:46:15 +0530

    keywordsinjava

---
+++ LearningJava/src/unit_01/P2_Task01_KeywordsInJava.java
@@ -0,0 +1,39 @@
+package unit_01;
+
+public class P2_Task01_KeywordsInJava {
+
+	public static void main(String[] args) {
+		//Variable Name 
+		//Syntax error on token "double", invalid VariableDeclaratorId
+		int double=12; 
+		
+		//Function Name 
+		//Syntax error on token "new", invalid AnnotationName
+		int new()
+		{
+			System.out.println("Keywords in java!!!");
+			return 0;
+		}  
+		//Object name 
+		//Syntax error on token "int", delete this token
+		Abc int =new Abc();
+	}
+
+}  
+    //Class Name
+    //Syntax error on token "int", Identifier expected
+    //keywords cannot use as class name
+    class int
+    {
+    	void main(String args)
+    	{
+    		System.out.println("Keywords in java!!!");
+    	}
+    }
+    class Abc
+    {
+    	void main(String args)
+    	{
+    		System.out.println("Keywords in java!!!");
+    	}
+    }
