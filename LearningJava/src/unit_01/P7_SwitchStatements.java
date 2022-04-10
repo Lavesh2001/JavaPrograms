d04900c4b7cb09e018bf75d288fb99a4f924aea0
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-04-10 10:46:23 +0530

    SwitchStatements

---
+++ LearningJava/src/unit_01/P7_SwitchStatements.java
@@ -0,0 +1,24 @@
+package unit_01;
+/* Problem Statements: P7_SwitchStatements:
+   Create a switch Statement [Manual],In which
+   a. When you pass 1 your program would print current year
+   b. When you pass 2 your program would print current month
+   c. When you pass 3 your program would print current day
+   d. When you Pass 3 your program would print Not applicable
+ 
+ */
+public class P7_SwitchStatements {
+
+	public static void main(String[] args) {
+		SwitchStatements obj=new SwitchStatements();
+		obj.switchStatements();
+	}
+
+}
+class SwitchStatements
+{
+	void switchStatements()
+	{
+		
+	}
+}
