87cf39c16dba52f1cef876ed7f131eb556e2f0e6
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-04-10 16:13:13 +0530

    Array Question

--- LearningJava/src/unit_01/P9_Array.java
+++ LearningJava/src/unit_01/P9_Array.java
@@ -60,7 +60,7 @@ class QuestionsOnArray {
 		System.out.print("\nDuplicates element in the array are:");
 		for(int i=0;i<arr.length;i++)
 		{
-			for(int j=i+1;j<arr.length;i++)
+			for(int j=i+1;j<arr.length;j++)
 			{
 				if(arr[i]==arr[j])
 				{
