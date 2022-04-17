242fc96dff2caf0f3ea776e21988b97f78363eb0
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-04-17 15:12:37 +0530

    Sample Class 1

--- LearningJava/src/unit_01/SampleClass1.java
+++ LearningJava/src/unit_01/SampleClass1.java
@@ -1,10 +1,11 @@
 package unit_01;
 
-public class SampleClass1 {
 
+public class SampleClass1 {
+        int a=5;
 	public static void main(String[] args) {
-		 int a=5;
-		 System.out.println(a);
+		
+
 
 	}
 
