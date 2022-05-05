59048a8434ecf00836239e6bac4917954bc360f2
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-05-05 23:13:30 +0530

    SamplecLass2

---
+++ LearningJava/src/unit_02/SampleClass2.java
@@ -0,0 +1,23 @@
+package unit_02;
+
+public class SampleClass2 {
+
+	
+	int a=20;
+	public int b=90;
+	private int c=200;
+	protected int d=34;
+	public static void main(String[] args) {
+	
+	}
+
+}
+class test1
+{
+	private String name="Lavesh";
+	protected int roll=5;
+	public int marks=100;
+	int avg=89;
+	
+}
+
