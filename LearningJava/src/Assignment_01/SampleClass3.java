9c28044e401c11229841b29cbb3d49a2ef5ac7a5
Lavesh Kashyap <Lavesh Kashyap@LAPTOP-IS38RD2V>
2022-05-05 23:13:15 +0530

    SampleClass3

--- LearningJava/src/Assignment_01/SampleClass3.java
+++ LearningJava/src/Assignment_01/SampleClass3.java
@@ -3,8 +3,16 @@ package Assignment_01;
 public class SampleClass3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+}
+class test3
+{
+	int a=10;
+	public int b=30;
+	private int c=23;
+	protected int d=90;
+	
 }
