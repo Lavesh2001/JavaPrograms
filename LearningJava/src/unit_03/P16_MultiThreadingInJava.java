ce8694909f7df489257b82be62a61d64ff5d0eb6
Lavesh Kashyap <lavesh@hotmail.com>
2022-05-17 11:01:08 +0530

    MultiThreadingInJava

---
+++ LearningJava/src/unit_03/P16_MultiThreadingInJava.java
@@ -0,0 +1,38 @@
+package unit_03;
+
+public class P16_MultiThreadingInJava {
+
+	public static void main(String[] args) {
+		
+		Threading obj=new Threading();
+		obj.threadConcept();
+		obj.threadSynchronization();
+		obj.interThreadCommunication();
+		obj.ProducerConsumersProblems();
+		obj.Wait_And_Notify();
+	}
+
+}
+class Threading
+{
+	void threadConcept()
+	{
+		
+	}
+	void threadSynchronization()
+	{
+		
+	}
+	void interThreadCommunication()
+	{
+		
+	}
+	void ProducerConsumersProblems()
+	{
+		
+	}
+	void Wait_And_Notify()
+	{
+		
+	}
+}
