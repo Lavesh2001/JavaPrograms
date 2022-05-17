beaa3449859f56f0fb838a3bfefe38195c88831d
Lavesh Kashyap <lavesh@hotmail.com>
2022-05-17 10:54:51 +0530

    FileHandlingInJava

---
+++ LearningJava/src/unit_03/P15_FileHandlingInJava.java
@@ -0,0 +1,30 @@
+package unit_03;
+
+public class P15_FileHandlingInJava {
+
+	public static void main(String[] args) {
+		
+		File obj=new File();
+		obj.takeInputFromAFileNotFromConsole();
+		obj.saveOutputIntoAFile();
+		obj.createALogFile();
+
+	}
+
+}
+
+class File
+{
+	void takeInputFromAFileNotFromConsole()
+	{
+		
+	}
+	void saveOutputIntoAFile()
+	{
+		
+	}
+	void createALogFile()
+	{
+		
+	}
+}
